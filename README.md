00bcbca031e3f3924bf7803e6957cf9c121f1a28
Waimea-Krclark <krclark@waimea.school.nz>
2025-03-20 12:25:52 +1300

    Starting 2d array

---
+++ README.md
@@ -0,0 +1,33 @@
+# Maze Game
+
+by Kieran Clark
+
+---
+
+This is a programming project for **NCEA Level 2**, assessed against standard [91896](docs/as91896.pdf).
+
+The project is written in the [Kotlin](https://kotlinlang.org) programming language and demonstrates a number of **advanced programming techniques**:
+- Modifying data stored in collections
+- Storing multi-dimensional data in collections
+- Creating functions that use parameters and/or return values
+- Using non-trivial string manipulation
+- Using additional non-core libraries
+
+
+
+---
+
+## Project Files
+
+- Program source code can be found in the [src](src/main/kotlin) folder
+- Program test plan is in [test-plan.md](docs/test-plan.md) in the docs folder
+- Program test evidence is in [test-results.md](docs/test-results.md) in the docs folder
+
+---
+
+## Project Description
+
+Maze game is a game about navigating a procedurally generated maze to reach the exit. It is a top down game where the player controls the character (@) using WASD. When the player reaches the exit door (D) a win condition is triggered.
+
+
+
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,16 @@
+/**
+ * =====================================================================
+ * Programming Project for NCEA Level 2, Standard 91896
+ * ---------------------------------------------------------------------
+ * Project Name:   MAZE GAME
+ * Project Author: Kieran Clark
+ * GitHub Repo:    https://github.com/Waimea-Krclark/level-2-programming-assessment
+ * ---------------------------------------------------------------------
+ * Notes:
+ * Procedurally generated top down maze game, player is controlled with WASD in a terminal GUI
+ * =====================================================================
+ */
+
 package org.example
 
 import com.varabyte.kotter.foundation.*
@@ -25,9 +38,21 @@ fun main() = session(){
     var screen = liveListOf<LiveList<String>>()
     screen.add(liveListOf(EDGE + HORIWALL.repeat(SCREENWIDTH) + EDGE) )
     repeat(SCREENHEIGHT){
-        screen.add(liveListOf(VERTWALL + EMPTY.repeat(SCREENWIDTH) + VERTWALL))
+        screen.add(liveListOf(VERTWALL))
+
     }
     screen.add(liveListOf(EDGE + HORIWALL.repeat(SCREENWIDTH) + EDGE) )
+
+    repeat(100){
+        val randomY = Random.nextInt(0, SCREENHEIGHT)
+        val randomX = Random.nextInt(0, SCREENWIDTH)
+        println(screen[1][0])
+       // if (screen[1][2] == EMPTY){
+//   screen[1][2] = HORIWALL
+       // }
+
+    }
+
 
     section {
         p{
@@ -36,7 +61,6 @@ fun main() = session(){
                     textLine(screenX)
                 }
             }
-
         }
     }.run {  }
 }
